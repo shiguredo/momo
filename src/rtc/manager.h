9a331125284388e7d2376fb85dfd36ec186b2942
tnoho <teaotya@gmail.com>
2019-02-10 18:51:24 +0900

    [WIP] カメラキャプチャー周りの M73 対応を行う

--- src/rtc/manager.h
+++ src/rtc/manager.h
@@ -1,6 +1,7 @@
 #ifndef RTC_MANAGER_H_
 #define RTC_MANAGER_H_
 #include "api/peerconnectioninterface.h"
+#include "pc/video_track_source.h"
 
 #include "connection.h"
 #include "connection_settings.h"
@@ -8,16 +9,16 @@
 class RTCManager
 {
 public:
-  RTCManager(ConnectionSettings conn_settings, std::unique_ptr<cricket::VideoCapturer> capturer);
+  RTCManager(ConnectionSettings conn_settings,
+             std::unique_ptr<rtc::VideoSourceInterface<webrtc::VideoFrame>> capturer);
   ~RTCManager();
-  static std::unique_ptr<cricket::VideoCapturer> createVideoCapturer();
   std::shared_ptr<RTCConnection> createConnection(
           webrtc::PeerConnectionInterface::RTCConfiguration rtc_config,
           RTCMessageSender *sender);
 
 private:
   rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface> _factory;
-  rtc::scoped_refptr<webrtc::VideoTrackSourceInterface> _video_source;
+  rtc::scoped_refptr<webrtc::VideoTrackSource> _video_source;
   std::unique_ptr<rtc::Thread> _networkThread;
   std::unique_ptr<rtc::Thread> _workerThread;
   std::unique_ptr<rtc::Thread> _signalingThread;
