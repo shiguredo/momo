78cc9a4e8c406eb1dbdac2e8890a96fc3213375e
tnoho <teaotya@gmail.com>
2019-05-23 01:11:37 +0900

    [WIP] M75 が Raspberry Pi で動いた

--- src/hwenc_il/il_h264_encoder.h
+++ src/hwenc_il/il_h264_encoder.h
@@ -23,7 +23,7 @@ extern "C"
 #include <list>
 
 #include "api/video_codecs/video_encoder.h"
-#include "rtc_base/criticalsection.h"
+#include "rtc_base/critical_section.h"
 #include "common_video/h264/h264_bitstream_parser.h"
 #include "common_video/include/bitrate_adjuster.h"
 #include "modules/video_coding/codecs/h264/include/h264.h"
@@ -41,24 +41,13 @@ public:
   int32_t InitEncode(const webrtc::VideoCodec *codec_settings,
                      int32_t number_of_cores,
                      size_t max_payload_size) override;
-
   int32_t RegisterEncodeCompleteCallback(
       webrtc::EncodedImageCallback *callback) override;
-
   int32_t Release() override;
-
   int32_t Encode(const webrtc::VideoFrame &frame,
-                 const webrtc::CodecSpecificInfo *codec_specific_info,
-                 const std::vector<webrtc::FrameType> *frame_types) override;
-
-  int32_t SetRateAllocation(const webrtc::VideoBitrateAllocation &bitrate_allocation,
-                            uint32_t framerate) override;
-
-  // Exposed for testing.
-  webrtc::H264PacketizationMode PacketizationModeForTesting() const
-  {
-    return packetization_mode_;
-  }
+                 const std::vector<webrtc::VideoFrameType> *frame_types) override;
+  void SetRates(const RateControlParameters& parameters) override;
+  webrtc::VideoEncoder::EncoderInfo GetEncoderInfo() const override;
 
 private:
   bool IsInitialized() const;
