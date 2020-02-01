e926bd0be560f1ff5f5821e03697f892026b880a
melpon <shigemasa7watanabe+github@gmail.com>
2020-02-02 05:06:15 +0900

    macOS での CMake ビルドが通った

---
+++ build/build.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+# 引数の処理
+
+PROGRAM="$0"
+
+_PACKAGES=" \
+  windows \
+  macos \
+  raspbian-buster_armv6 \
+  raspbian-buster_armv7 \
+  ubuntu-16.04_armv7_ros \
+  ubuntu-16.04_x86_64_ros \
+  ubuntu-18.04_armv8 \
+  ubuntu-18.04_armv8_jetson_nano \
+  ubuntu-18.04_x86_64 \
+"
+
+function show_help() {
+  echo "$PROGRAM [--clean] [--package] <package>"
+  echo "<package>:"
+  for package in $_PACKAGES; do
+    echo "  - $package"
+  done
+}
+
+PACKAGE=""
+FLAG_CLEAN=0
+FLAG_PACKAGE=0
+
+while [ $# -ne 0 ]; do
+  case "$1" in
+    "--clean" ) FLAG_CLEAN=1 ;;
+    "--package" ) FLAG_PACKAGE=1 ;;
+    --* )
+      show_help
+      exit 1
+      ;;
+    * )
+      if [ -n "$PACKAGE" ]; then
+        show_help
+        exit 1
+      fi
+      PACKAGE="$1"
+      ;;
+  esac
+  shift 1
+done
+
+_FOUND=0
+for package in $_PACKAGES; do
+  if [ "$PACKAGE" = "$package" ]; then
+    _FOUND=1
+    break
+  fi
+done
+
+if [ $_FOUND -eq 0 ]; then
+  show_help
+  exit 1
+fi
+
+echo "--clean: " $FLAG_CLEAN
+echo "--package: " $FLAG_PACKAGE
+echo "<package>: " $PACKAGE
+
+set -ex
+
+pushd ..
+  MOMO_COMMIT="$(git rev-parse HEAD)"
+popd
+
+case "$PACKAGE" in
+  "windows" )
+    echo "Windows では build.bat を利用してください。"
+    exit 1
+    ;;
+  "macos" )
+    if [ $FLAG_CLEAN -eq 1 ]; then
+      rm -rf macos/_source
+      rm -rf macos/_build
+      rm -rf macos/_install
+      exit 0
+    fi
+
+    ./macos/install_deps.sh
+
+    if [ -z "$JOBS" ]; then
+      JOBS=`sysctl -n hw.logicalcpu_max`
+      if [ -z "$JOBS" ]; then
+        JOBS=1
+      fi
+    fi
+
+    mkdir -p ../_build/$PACKAGE
+    pushd ../_build/$PACKAGE
+      cmake \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DMOMO_PACKAGE_NAME="macos" \
+        -DMOMO_VERSION="$MOMO_VERSION" \
+        -DMOMO_COMMIT="$MOMO_COMMIT" \
+        -DWEBRTC_BUILD_VERSION="$WEBRTC_BUILD_VERSION" \
+        -DWEBRTC_READABLE_VERSION="$WEBRTC_READABLE_VERSION" \
+        -DWEBRTC_COMMIT="$WEBRTC_COMMIT" \
+        ../..
+      cmake --build . -j$JOBS
+    popd
+
+    if [ $FLAG_PACKAGE -eq 1 ]; then
+      echo "TODO"
+    fi
+
+    ;;
+  * )
+    echo "TODO"
+    ;;
+esac
---
+++ build/macos/install_deps.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+cd "`dirname $0`"
+
+set -ex
+
+SOURCE_DIR="`pwd`/_source"
+BUILD_DIR="`pwd`/_build"
+INSTALL_DIR="`pwd`/_install"
+
+mkdir -p $SOURCE_DIR
+mkdir -p $BUILD_DIR
+mkdir -p $INSTALL_DIR
+
+source ../../VERSION
+
+if [ -z "$JOBS" ]; then
+  JOBS=`sysctl -n hw.logicalcpu_max`
+  if [ -z "$JOBS" ]; then
+    JOBS=1
+  fi
+fi
+
+# CLI11
+if [ ! -e $INSTALL_DIR/CLI11/include/CLI/Version.hpp ]; then
+  pushd $INSTALL_DIR
+    rm -rf CLI11
+    git clone --branch v$CLI11_VERSION --depth 1 https://github.com/CLIUtils/CLI11.git
+  popd
+fi
+
+# nlohmann/json
+if [ ! -e $INSTALL_DIR/json/include/nlohmann/json.hpp ]; then
+  pushd $INSTALL_DIR
+    rm -rf json
+    git clone --branch v$JSON_VERSION --depth 1 https://github.com/nlohmann/json.git
+  popd
+fi
+
+# WebRTC
+if [ ! -e $INSTALL_DIR/webrtc/lib/libwebrtc.a ]; then
+  rm -rf $INSTALL_DIR/webrtc
+  ../../script/get_webrtc.sh $WEBRTC_BUILD_VERSION macos $INSTALL_DIR
+fi
+
+# LLVM
+if [ ! -e $INSTALL_DIR/llvm/clang/bin/clang++ ]; then
+  rm -rf $INSTALL_DIR/llvm
+  ../../script/get_llvm.sh $INSTALL_DIR/webrtc $INSTALL_DIR
+fi
+
+# Boost
+if [ ! -e $INSTALL_DIR/boost/lib/libboost_filesystem.a ]; then
+  rm -rf $SOURCE_DIR/boost
+  rm -rf $BUILD_DIR/boost
+  rm -rf $INSTALL_DIR/boost
+  mkdir -p $SOURCE_DIR/boost
+  ../../script/setup_boost.sh $BOOST_VERSION $SOURCE_DIR/boost
+  pushd $SOURCE_DIR/boost/source
+    echo "using clang : : $INSTALL_DIR/llvm/clang/bin/clang++ : ;" > project-config.jam
+    SYSROOT="`xcrun --sdk macosx --show-sdk-path`"
+    ./b2 \
+      cflags=" \
+        --sysroot=$SYSROOT \
+      " \
+      cxxflags=" \
+        -isystem $INSTALL_DIR/llvm/libcxx/include \
+        -nostdinc++ \
+        --sysroot=$SYSROOT \
+      " \
+      toolset=clang \
+      visibility=hidden \
+      link=static \
+      variant=release \
+      install \
+      -j$JOBS \
+      --build-dir=$BUILD_DIR/boost \
+      --prefix=$INSTALL_DIR/boost \
+      --ignore-site-config \
+      --with-filesystem
+  popd
+fi
+
+# SDL2
+if [ ! -e $INSTALL_DIR/SDL2/lib/libSDL2.a ]; then
+  rm -rf $SOURCE_DIR/SDL2
+  rm -rf $BUILD_DIR/SDL2
+  rm -rf $INSTALL_DIR/SDL2
+  mkdir -p $SOURCE_DIR/SDL2
+  mkdir -p $BUILD_DIR/SDL2
+  ../../script/setup_sdl2.sh $SDL2_VERSION $SOURCE_DIR/SDL2
+  pushd $BUILD_DIR/SDL2
+    CC=$INSTALL_DIR/llvm/clang/bin/clang \
+    CXX=$INSTALL_DIR/llvm/clang/bin/clang++ \
+    cmake \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR/SDL2 \
+      $SOURCE_DIR/SDL2/source
+
+    cmake --build . -j$JOBS
+    cmake --build . --target install
+  popd
+fi
