73a1e9daf5d751b04a61d7c6dd8df6119f35847f
melpon <shigemasa7watanabe+github@gmail.com>
2020-03-16 14:41:01 +0900

    アライメントが揃ってる場合と揃ってない場合で NAL のサイズを変える

--- src/hwenc_nvcodec/nvcodec_h264_encoder.cpp
+++ src/hwenc_nvcodec/nvcodec_h264_encoder.cpp
@@ -271,8 +271,9 @@ int32_t NvCodecH264Encoder::Encode(
       }
       if (data == 0x01 && zero_count >= 2) {
         if (nal_start_idx != 0) {
-          nals.push_back({nal_start_idx, i - nal_start_idx + 1 - 4});
-          //printf(" nal_size: %d ", i - nal_start_idx + 1 - 4);
+          nals.push_back({nal_start_idx,
+                          i - nal_start_idx + 1 - (zero_count == 2 ? 3 : 4)});
+          //printf(" nal_size: %d ", i - nal_start_idx + 1 - (zero_count == 2 ? 3 : 4));
         }
         nal_start_idx = i + 1;
         //printf(" nal_start_idx: %d\n", nal_start_idx);
