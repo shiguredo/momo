a6c11a85c3de6a4572f1bde0d02aa3586d3e0736
melpon <shigemasa7watanabe+github@gmail.com>
2020-06-16 11:39:21 +0900

    必要な部分を消してしまってた

--- script/init_rootfs_arm64.sh
+++ script/init_rootfs_arm64.sh
@@ -13,7 +13,10 @@ set -ex
 multistrap --no-auth -a arm64 -d $TARGET_DIR -f $CONFIG_FILE
 find $TARGET_DIR/usr/lib/aarch64-linux-gnu -lname '/*' -printf '%p %l\n' | while read link target; do ln -snfv "../../..${target}" "${link}"; done
 find $TARGET_DIR/usr/lib/aarch64-linux-gnu/pkgconfig -printf "%f\n" | while read target; do ln -snfv "../../lib/aarch64-linux-gnu/pkgconfig/${target}" $TARGET_DIR/usr/share/pkgconfig/${target}; done
-if [ -L $TARGET_DIR/usr/lib/gcc/aarch64-linux-gnu/7/libgcc_s.so ]; then
+if [ -L $TARGET_DIR/usr/lib/gcc/aarch64-linux-gnu/5/libgcc_s.so ]; then
+  unlink $TARGET_DIR/usr/lib/gcc/aarch64-linux-gnu/5/libgcc_s.so
+  ln -s ../../../../../lib64/aarch64-linux-gnu/libgcc_s.so.1 $TARGET_DIR/usr/lib/gcc/aarch64-linux-gnu/5/libgcc_s.so
+elif [ -L $TARGET_DIR/usr/lib/gcc/aarch64-linux-gnu/7/libgcc_s.so ]; then
   unlink $TARGET_DIR/usr/lib/gcc/aarch64-linux-gnu/7/libgcc_s.so
   ln -s ../../../../../lib64/aarch64-linux-gnu/libgcc_s.so.1 $TARGET_DIR/usr/lib/gcc/aarch64-linux-gnu/7/libgcc_s.so
 elif [ -L $TARGET_DIR/usr/lib/gcc/aarch64-linux-gnu/8/libgcc_s.so ]; then
