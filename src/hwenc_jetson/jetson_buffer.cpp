358e6fb1a9be190c4156d7cbdd39d112edcacb45
tnoho <teaotya@gmail.com>
2020-08-28 21:30:29 +0900

    ToI420 を動くようにする

--- src/hwenc_jetson/jetson_buffer.cpp
+++ src/hwenc_jetson/jetson_buffer.cpp
@@ -6,6 +6,7 @@
 // WebRTC
 #include <api/video/i420_buffer.h>
 #include <rtc_base/logging.h>
+#include <third_party/libyuv/include/libyuv.h>
 
 static const int kBufferAlignment = 64;
 
@@ -62,8 +63,62 @@ int JetsonBuffer::height() const {
 
 rtc::scoped_refptr<webrtc::I420BufferInterface> JetsonBuffer::ToI420() {
   rtc::scoped_refptr<webrtc::I420Buffer> i420_buffer =
+      webrtc::I420Buffer::Create(raw_width_, raw_height_);
+  if (video_type_ == webrtc::VideoType::kMJPEG) {
+    NvBufferParams params = {0};
+    if (NvBufferGetParams(fd_, &params) == -1) {
+      RTC_LOG(LS_ERROR) << __FUNCTION__ << " Failed to NvBufferGetParams";
+      return i420_buffer;
+    }
+    int ret;
+    void *data_addr;
+    uint8_t* dest_addr;
+    for (int plane = 0; plane < params.num_planes; plane++) {
+      ret = NvBufferMemMap (fd_, plane, NvBufferMem_Read, &data_addr);
+      if (ret == 0) {
+        int height;
+        int v_stride;
+        if (plane == 0) {
+          dest_addr = i420_buffer.get()->MutableDataY();
+          height = raw_height_;
+          v_stride = 1;
+        } else if (plane == 1) {
+          dest_addr = i420_buffer.get()->MutableDataU();
+          height = (raw_height_ + 1) >> 1;
+          v_stride = 2;
+        } else if (plane == 2) {
+          dest_addr = i420_buffer.get()->MutableDataV();
+          height = (raw_height_ + 1) >> 1;
+          v_stride = 2;
+        }
+        for (int i = 0; i < height; i++)
+        {
+          memcpy(dest_addr + params.width[plane] * i, 
+                (uint8_t*)data_addr + params.pitch[plane] * i * v_stride,
+                params.width[plane]);
+        }
+      }
+      NvBufferMemUnMap (fd_, plane, &data_addr);
+      if (ret == -1) {
+        RTC_LOG(LS_ERROR) << __FUNCTION__ << " Failed to NvBufferMemMap plane=" << plane;
+        return i420_buffer;
+      }
+    }
+  } else {
+    const int conversionResult = libyuv::ConvertToI420(
+        data_.get(), length_, i420_buffer.get()->MutableDataY(),
+        i420_buffer.get()->StrideY(), i420_buffer.get()->MutableDataU(),
+        i420_buffer.get()->StrideU(), i420_buffer.get()->MutableDataV(),
+        i420_buffer.get()->StrideV(), 0, 0, raw_width_, raw_height_, raw_width_,
+        raw_height_, libyuv::kRotate0, ConvertVideoType(video_type_));
+  }
+  if (raw_width_ == scaled_width_ && raw_height_ == scaled_height_) {
+    return i420_buffer;
+  }
+  rtc::scoped_refptr<webrtc::I420Buffer> scaled_buffer =
       webrtc::I420Buffer::Create(scaled_width_, scaled_height_);
-  return i420_buffer;
+  scaled_buffer->ScaleFrom(*i420_buffer->ToI420());
+  return scaled_buffer;
 }
 
 int JetsonBuffer::RawWidth() const {
