34fe4a43aa271a9e2539b4363e36e901870e3b8b
melpon <shigemasa7watanabe+github@gmail.com>
2020-11-23 20:38:13 +0900

    修正

--- src/hwenc_nvcodec/nvcodec_h264_encoder.cpp
+++ src/hwenc_nvcodec/nvcodec_h264_encoder.cpp
@@ -283,7 +283,7 @@ int32_t NvCodecH264Encoder::Encode(
         webrtc::H264PacketizationMode::NonInterleaved;
 
     h264_bitstream_parser_.ParseBitstream(packet.data(), packet.size());
-    h264_bitstream_parser_.GetLastSliceQp(&encoded_image_->qp_);
+    h264_bitstream_parser_.GetLastSliceQp(&encoded_image_.qp_);
 
     webrtc::EncodedImageCallback::Result result =
         callback_->OnEncodedImage(encoded_image_, &codec_specific);
