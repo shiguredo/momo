a39fcf420d8aa29738dce00686eb1b21b7d0f74e
melpon <shigemasa7watanabe+github@gmail.com>
2023-05-29 16:27:06 +0900

    VPL の Init を毎回呼ぶように修正

--- src/hwenc_msdk/msdk_video_decoder.cpp
+++ src/hwenc_msdk/msdk_video_decoder.cpp
@@ -79,7 +79,9 @@ std::unique_ptr<MFXVideoDECODE> MsdkVideoDecoder::CreateDecoder(
   //            << std::endl;
   //}
 
-  if (init) {
+  // Query した上で Init しても MFX_ERR_UNSUPPORTED になることがあるので
+  // 本来 Init が不要な時も常に呼ぶようにして確認する
+  /*if (init)*/ {
     // Initialize the Media SDK encoder
     sts = decoder->Init(&param);
     if (sts != MFX_ERR_NONE) {
