57511b4e3cf58c2167d960208e15f5eeeb207113
melpon <shigemasa7watanabe+github@gmail.com>
2023-08-11 11:40:47 +0900

    コード整理

--- src/hwenc_v4l2/v4l2_h264_encoder.h
+++ src/hwenc_v4l2/v4l2_h264_encoder.h
@@ -1,14 +1,3 @@
-/*
- *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree. An additional intellectual property rights grant can be found
- *  in the file PATENTS.  All contributing project authors may
- *  be found in the AUTHORS file in the root of the source tree.
- *
- */
-
 #ifndef V4L2_H264_ENCODER_H_
 #define V4L2_H264_ENCODER_H_
 
