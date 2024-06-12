db5ff26cdbae8bb002a147cfa1a7e793570e0d8a
melpon <shigemasa7watanabe+github@gmail.com>
2024-06-13 01:06:51 +0900

    修正

--- src/rtc/momo_video_encoder_factory.cpp
+++ src/rtc/momo_video_encoder_factory.cpp
@@ -266,8 +266,8 @@ std::unique_ptr<webrtc::VideoEncoder> MomoVideoEncoderFactory::Create(
 #if defined(__APPLE__)
     if (config_.h264_encoder == VideoCodecInfo::Type::VideoToolbox) {
       return WithSimulcast(format,
-                           [this](const webrtc::SdpVideoFormat& format) {
-                             return video_encoder_factory_->Create(format);
+                           [this, env](const webrtc::SdpVideoFormat& format) {
+                             return video_encoder_factory_->Create(env, format);
                            });
     }
 #endif
