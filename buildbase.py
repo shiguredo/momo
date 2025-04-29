c1ebb73457df5b4750a7fa591819fb3e6fc7bc8d
voluntas <nakai@shiguredo.jp>
2025-04-29 15:22:42 +0900

    boost のダウンロードを時雨堂ミラーに切り替える

--- buildbase.py
+++ buildbase.py
@@ -690,8 +690,10 @@ def build_and_install_boost(
 ):
     version_underscore = version.replace(".", "_")
     archive = download(
-        # XXX: ここ GitHub Releases に変更するのはダメなのだろうか？
-        f"https://archives.boost.io/release/{version}/source/boost_{version_underscore}.tar.gz",
+        # 公式サイトに負荷をかけないための時雨堂による R2 ミラー
+        f"https://pub-31f2ce80536a4322af33196843feefc2.r2.dev/boost_{version_underscore}.tar.gz",
+        # Boost 公式のミラー
+        # f"https://archives.boost.io/release/{version}/source/boost_{version_underscore}.tar.gz",
         source_dir,
     )
     extract(archive, output_dir=build_dir, output_dirname="boost")
