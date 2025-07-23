13d774252d0aaaa0251311ac497ce285fdb73428
melpon <shigemasa7watanabe+github@gmail.com>
2025-07-24 01:56:52 +0900

    修正

--- src/hwenc_v4l2/v4l2_h264_decoder.cpp
+++ src/hwenc_v4l2/v4l2_h264_decoder.cpp
@@ -41,7 +41,7 @@ int32_t V4L2H264Decoder::Decode(const webrtc::EncodedImage& input_image,
 
   decoder_->Decode(
       input_image.data(), input_image.size(), input_image.RtpTimestamp(),
-      [this](rtc::scoped_refptr<webrtc::VideoFrameBuffer> buffer,
+      [this](webrtc::scoped_refptr<webrtc::VideoFrameBuffer> buffer,
              int64_t timestamp_rtp) {
         webrtc::VideoFrame decoded_image = webrtc::VideoFrame::Builder()
                                                .set_video_frame_buffer(buffer)
